f06692b29b711f611072f42989c34478f4b497f1
unknown <kmm@rmlabs.net>
2009-02-11 04:13:05 -0800

    .

--- shell/shell.py
+++ shell/shell.py
@@ -1,19 +1,54 @@
 import readline
 
-
 class Shell:
-    commands = [("demo", "demo")]
+    commands = {}
+    trace = [] 
+
     prompt = "> "
+    exitcmd = "exit"
 
     def __init__(self):
-        pass
+        self.commands['test'] = self.default
+        self.commands['!'] = self.poptrace
+
+    def poptrace(self, input):
+        if len(self.trace) == 0:
+            return "No exceptions on stack."
+        else:
+            return self.trace.pop()
 
     def completer(self, input, state):
-        options = [cmd[0] for cmd in self.commands if cmd[0].startswith(input)]
+        options = [cmd for cmd in self.commands.keys() if cmd.startswith(input)]
         try:
             return options[state]
         except IndexError:
             return None
+
+    def handle(self, input):
+        if not input:
+            return
+
+        try:
+            (cmd, args) = input.split(' ', 1)
+        except ValueError:
+            cmd = input
+            args = None
+
+        if cmd in self.commands.keys():
+            try:
+                return self.commands[cmd](args)
+            except AttributeError or NameError, e:
+                # lists have pop() but no push()! wtf?
+                self.trace.insert(0, e)
+                return "Unknown command '%s'" % cmd
+            except TypeError, e:
+                self.trace.insert(0, e)
+                return "Bad argument '%s' for '%s'" % (args, cmd)
+        else:
+            return self.default(input)
+
+    def default(self, input):
+        return input
 
     def start(self):
         readline.set_completer(self.completer)
@@ -21,8 +56,26 @@ class Shell:
 
         while True:
             input = raw_input(self.prompt)
-            print input
 
+            if input == self.exitcmd:
+                break
+
+            result = self.handle(input)            
+            if result:
+                print result
+            else:
+                print input
+        
+        return True
 
 s = Shell()
+
+def subshell(parent):
+    ss = Shell()
+    ss.prompt = "subshell> "
+    ss.exitcmd = "z"
+    return ss.start()
+
+s.commands['sqr'] = lambda x: int(x) ** int(x)
+s.commands['subshell'] = subshell
 s.start()
