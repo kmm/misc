750e6d634667a0d1675b0ff46a1b561ff242f331
kmm <kmm@rmlabs.net>
2009-02-12 20:18:04 +0000

    SPyS initial commit

---
+++ spys/calc.py
@@ -0,0 +1,94 @@
+import spys
+
+class Calc(spys.SPyShell):
+    """
+    Implements a simple four function RPN calculator using SPyS.
+    
+    This demonstration implements a simple RPN calculator by
+    inheriting from the SPyS interactive shell library.
+    At the 'rpn>' prompt, enter a valid postfix-notation expression
+    for evaluation.
+
+    RPN commands (* indicates stack side effects):
+          = -> Display the top element on the stack
+          $ -> Display the entire stack
+          x -> Clear the stack*
+          ^ -> Pop the top element off the stack*
+    +,-,*,/ -> Pop the top two operands off the top of the stack as (a, b)
+               and perform (a opr b), pushing the result back on to the stack.*
+
+    Example:
+    rpn> 1 1 2 3 5 8 + - * / =
+    0: -0.05
+    """
+    def __init__(self, arg=None):
+        super(Calc, self).__init__(self)
+        self.stack = []
+        self.setprompt("rpn> ")
+        self.start()
+    
+    def dumpstack(self, limit=None):
+        """Pretty-print RPN stack, with indexes."""
+        output = []
+        if self.stack:
+            index = 0
+            for item in reversed(self.stack):
+                output.append("%s: %s" % (index, item))
+                index += 1
+                if limit and (index >= limit):
+                    break
+            return "\n".join(output)
+        else:
+            return "Empty stack"
+
+    def default(self, arg):
+        """
+        Main program loop, overrides SPyS default command handler
+        method to implement interactive RPN calculator.
+        """
+        operands = "+-*/" # operator list, must be python operators
+        buffer = [] # buffer for messages
+
+        for word in arg.split(' '):
+            if word == 'x':
+                self.stack = []
+                buffer.append(self.dumpstack())
+            elif word == '^':
+                return self.stack.pop()
+            elif word in operands and (len(self.stack) >= 2):
+                try:
+                    b = str(self.stack.pop())
+                    a = str(self.stack.pop())
+                    self.stack.append(eval(a + word + b))
+                except e:
+                    return "Bad mojo"
+            elif word == '$':
+                buffer.append(self.dumpstack())
+            elif word == '=':
+                buffer.append(self.dumpstack(1))
+            else:
+                try:
+                    self.stack.append(float(word))
+                except ValueError:
+                    return "Input must be an operator or a parseable number"
+        return "\n\n".join(buffer)
+
+def ponies(arg):
+    """Plain functions can be dynaloaded as well"""
+    return "OMG PONIES!"
+
+def spys_exports():
+    """
+    spys_exports() describes module functionality to allow dynaloading by another SPyS REPL 
+    instance using the @load command. Structure should be a list of tuples, each tuple
+    containing the package name, the command name that will be exported to the loading
+    SPyS environment, and finally the callable object itself. When @load <package> is
+    called, the presence of this function will indicate this package is a SPyS plugin
+    and will allow the parent to load and map the exported callables into itself.
+    """
+    return [(__name__, "calc", Calc), 
+            (__name__, "ponies", ponies)]
+
+if __name__ == "__main__":
+    calc = Calc()
+    calc.start()
