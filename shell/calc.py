b9c81045c5e9eade84f4176a330f5fc0f1e7215c
kmm <kmm@rmlabs.net>
2009-02-12 17:07:08 +0000

    Added dynaload and help functionality

--- shell/calc.py
+++ shell/calc.py
@@ -4,34 +4,32 @@ class Calc(shell.Shell):
     def __init__(self, arg):
         shell.Shell.__init__(self)
         self.stack = []
-        self.prompt = "calc> "
-        self.setcmd('>', self.stackpop)
+        self.setprompt("rpn> ")
         self.start()
 
-        def default(self, arg):
-            operands = "+-*/"
-            if arg.empty:
-                return self.stack
+    def default(self, arg):
+        operands = "+-*/"
 
-            for word in arg.split(' '):
-                if word in operands and (len(self.stack) >= 2):
-                    try:
-                        b = str(self.stack.pop())
-                        a = str(self.stack.pop())
-                        print a + word + b
-                        self.stack.append(eval(a + word + b))
-                    except e:
-                        return "Bad mojo"
-                else:
-                    try:
-                        self.stack.append(float(word))
-                    except ValueError:
-                        return "Input must be an operator or a parseable number"
+        for word in arg.split(' '):
+            if word in operands and (len(self.stack) >= 2):
+                try:
+                    b = str(self.stack.pop())
+                    a = str(self.stack.pop())
+                    print a + word + b
+                    self.stack.append(eval(a + word + b))
+                except e:
+                    return "Bad mojo"
+            else:
+                try:
+                    self.stack.append(float(word))
+                except ValueError:
+                    return "Input must be an operator or a parseable number"
             
-            return self.stack
+        return self.stack
 
-        def stackpop(self, arg):
-            try:
-                return self.stack.pop()
-            except IndexError:
-                return None
+def ponies():
+    return "OMG PONIES!"
+
+def exports():
+    return [(__name__, "CALC", Calc), 
+            (__name__, "ponies", ponies)]
--- shell/shell.py
+++ shell/shell.py
@@ -1,41 +1,79 @@
 import readline
-import pdb
- 
+
 class Shell:
     __commands = None
-    trace = None
- 
-    prompt = None
-    exitcmd = None
+    __cmdhelp = None
+    __trace = None
+    __prompt = None
+    __exitcmds = None
  
     def __init__(self):
         self.__commands = {}
-        self.__commands['!'] = self.poptrace
+        self.__cmdhelp = {}
+        self.setcmd('@!', self.poptrace, "- Pops last exception off trace stack")
+        self.setcmd('@load', self.loadmodule, "<module> - Imports an extension")
+        self.setcmd('@>', self.setprompt, "[string] - Sets interactive prompt to string, default if blank")
+        self.setcmd('?', self.help, "- Displays help message")
 
-        self.trace = []
+        self.__trace = []
+        self.__exitcmds = ['exit', 'quit']
+
+        self.setprompt()
+
+    def help(self, arg):
+        if not arg:
+            return "? <command> - Displays command help, if available\nAvailable commands: %s" % ", ".join(self.__commands.keys())    
+        if (arg in self.__commands) and (arg in self.__cmdhelp) and (self.__cmdhelp[arg]):
+            return "%s %s" % (arg, self.__cmdhelp[arg])
+        if arg not in self.__commands:
+            return "%s is unbound" % arg
         
-        self.prompt = "> "
-        self.exitcmd = "exit"
+        return "No help for %s" % arg
 
-    def setcmd(self, keyword, fn):
+    def setcmd(self, keyword, fn, help=None):
         if keyword and not fn:
             del(__commands[keyword])
+            del(__cmdhelp[keyword])
         if keyword and fn:
             self.__commands[keyword] = fn
+            self.__cmdhelp[keyword] = help
 
+    def setprompt(self, input=None):
+        if input:
+            self.__prompt = input
+        else:
+            self.__prompt = "> "
 
     def poptrace(self, input):
-        if len(self.trace) == 0:
+        if len(self.__trace) == 0:
             return "No exceptions on stack."
         else:
-            return self.trace.pop()
+            return self.__trace.pop()
+
+    def loadmodule(self, input):
+        commands = []
+        try:
+            mod =__import__(input)
+            if 'exports' in dir(mod):
+                exportlist = mod.exports()
+                for export in exportlist:
+                    self.setcmd(export[1], export[2])
+                    commands.append(export[1])
+                msg = "%s new commands imported from %s [%s]" % (len(commands), mod.__name__, ','.join(commands))
+                return msg 
+            else:
+                return "Could not enumerate module exports in %s" % mod.__name__
+        except ImportError, e:
+            return "Load failed, couldn't import %s" % input
  
     def completer(self, input, state):
         options = [c for c in self.__commands.keys() if c.startswith(input)]
+        opt = None
         try:
-            return options[state]
+            opt = options[state]
         except IndexError:
-            return None
+            opt = None
+        return opt
  
     def handle(self, input):
         if not input:
@@ -50,11 +88,12 @@ class Shell:
             try:
                 return self.__commands[cmd](args)
             except AttributeError or NameError, e:
-                # lists have pop() but no push()! wtf?
-                self.trace.append(e)
+                print e
+                self.__trace.append(e)
                 return "Command '%s' failed" % cmd
             except TypeError, e:
-                self.trace.append(e)
+                print e
+                self.__trace.append(e)
                 return "Bad argument '%s' for '%s'" % (args, cmd)
         else:
             return self.default(input)
@@ -68,9 +107,9 @@ class Shell:
 
         while True:
             try:
-                input = raw_input(self.prompt)
+                input = raw_input(self.__prompt)
                 
-                if input == self.exitcmd:
+                if input in self.__exitcmds:
                     break
  
                 result = self.handle(input)
@@ -80,50 +119,11 @@ class Shell:
                     print input
             except KeyboardInterrupt:
                 break
-
-
         return ret
  
-
-
 if __name__ == "__main__":
     s = Shell()
-    
-    class Calc(Shell):
-        def __init__(self, arg):
-            Shell.__init__(self)
-            self.stack = []
-            self.prompt = "calc> "
-            self.setcmd('>', self.stackpop)
-            self.start()
-
-        def default(self, arg):
-            operands = "+-*/"
-            if arg.empty:
-                return self.stack
-
-            for word in arg.split(' '):
-                if word in operands and (len(self.stack) >= 2):
-                    try:
-                        b = str(self.stack.pop())
-                        a = str(self.stack.pop())
-                        print a + word + b
-                        self.stack.append(eval(a + word + b))
-                    except e:
-                        return "Bad mojo"
-                else:
-                    try:
-                        self.stack.append(float(word))
-                    except ValueError:
-                        return "Input must be an operator or a parseable number"
-            
-            return self.stack
-
-        def stackpop(self, arg):
-            try:
-                return self.stack.pop()
-            except IndexError:
-                return None
+    import calc
 
     def subshell(arg):
         ss = Shell()
@@ -131,8 +131,9 @@ if __name__ == "__main__":
         ss.setcmd('hex', lambda x: "0x%x" % int(x))
         ss.start("Subshell")
 
-    s.setcmd('sqr', lambda x: int(x) * int(x))
+    s.setcmd('sqr', lambda x: int(x) * int(x), "<x> - Returns x*x")
     s.setcmd('subshell', subshell)
-    s.setcmd('calc', Calc)
+    s.setcmd('calc', calc.Calc)
+    s.setprompt('test> ')
     s.start()
 
